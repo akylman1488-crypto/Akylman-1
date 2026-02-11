ec45c6c44138763c4dd45c50a62bb35073a04adb
akylman1488-crypto <akylman1488@gmail.com>
2026-02-11 09:33:30 +0000

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -2,9 +2,8 @@ import streamlit as st
 import json
 
 def save_chat_to_json():
-    if st.session_state.messages:
-        chat_data = json.dumps(st.session_state.messages, ensure_ascii=False, indent=2)
-        return chat_data
+    if "messages" in st.session_state and st.session_state.messages:
+        return json.dumps(st.session_state.messages, ensure_ascii=False, indent=2)
     return None
 
 def download_chat_button():
