0dd05f07270fe1f820a25c298b43a440d12eed63
akylman1488-crypto <akylman1488@gmail.com>
2026-02-12 10:10:24 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,7 @@
-import streamlit as st
-from duckduckgo_search import DDGS
 from pypdf import PdfReader
-from docx import Document
 
 def extract_text(file):
-    try:
-        if file.type == "application/pdf":
-            reader = PdfReader(file)
-            return "".join([page.extract_text() for page in reader.pages])
-        elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            doc = Document(file)
-            return "\n".join([p.text for p in doc.paragraphs])
-        else:
-            return file.read().decode("utf-8")
-    except:
-        return "Ошибка чтения файла."
-
-def search_web(query):
-    try:
-        with DDGS() as ddgs:
-            return "\n".join([r['body'] for r in ddgs.text(query, max_results=3)])
-    except: return ""
+    if not file: return ""
+    if file.type == "application/pdf":
+        return " ".join([p.extract_text() for p in PdfReader(file).pages])
+    return str(file.read(), "utf-8")
