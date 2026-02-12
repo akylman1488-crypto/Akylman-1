d3390dd8f6c643647a175220bb721dd8d07379d4
akylman1488-crypto <akylman1488@gmail.com>
2026-02-12 10:13:26 +0600

    Update timer_module.py

--- timer_module.py
+++ timer_module.py
@@ -1,15 +1,4 @@
 import streamlit as st
 import time
-
-def study_timer():
-    if 'start_time' not in st.session_state:
-        st.session_state.start_time = None
-
-    if st.button("⏱ Запустить таймер на 25 мин"):
-        st.session_state.start_time = time.time()
-        st.success("Таймер запущен!")
-
-    if st.session_state.start_time:
-        elapsed = time.time() - st.session_state.start_time
-        remaining = max(0, 1500 - int(elapsed))
-        st.sidebar.write(f"Осталось: {remaining // 60}:{remaining % 60:02d}")
+def start_timer():
+    st.write("Фокус: 5 минут")
