a95eb691aba9f6bd917e7c6d5100901a8b3e03cc
akylman1488-crypto <akylman1488@gmail.com>
2026-02-12 10:10:38 +0600

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,16 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-def create_chart():
-    st.subheader("Построение графика")
-    data_input = st.text_area("Введи данные (Год, Значение) через запятую:", "2020,10\n2021,15\n2022,20")
-    if st.button("Построить"):
-        try:
-            # Простая логика парсинга
-            lines = data_input.split('\n')
-            data = [line.split(',') for line in lines]
-            df = pd.DataFrame(data, columns=['X', 'Y'])
-            st.line_chart(df)
-        except:
-            st.error("Ошибка данных")
+def draw_chart(data_str):
+    try:
+        df = pd.DataFrame([x.split(',') for x in data_str.split('\n')], columns=['X', 'Y'])
+        st.line_chart(df.set_index('X'))
+    except: st.error("Ошибка формата данных")
