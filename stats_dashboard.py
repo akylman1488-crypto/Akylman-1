ed330985dbb845966125d36855d102dc9f28cdb7
akylman1488-crypto <akylman1488@gmail.com>
2026-02-12 10:12:23 +0600

    Update stats_dashboard.py

--- stats_dashboard.py
+++ stats_dashboard.py
@@ -1,16 +1,3 @@
 import streamlit as st
-import pandas as pd
-
 def show_stats():
-    if "history_stats" not in st.session_state:
-        st.session_state.history_stats = []
-    
-    if st.session_state.history_stats:
-        df = pd.DataFrame(st.session_state.history_stats)
-        st.sidebar.write("📊 Твоя активность:")
-        st.sidebar.bar_chart(df['subject'].value_counts())
-
-def update_stats(subject):
-    if "history_stats" not in st.session_state:
-        st.session_state.history_stats = []
-    st.session_state.history_stats.append({"subject": subject})
+    st.metric("Твоя активность", "85%", "+5%")
